7a6e069c77d2a6db05beb4732220a1445a5e307d
maksym.slipchyshyn <maksym.slipchyshyn@CHICAGO-12>
2025-12-06 09:10:22 +0100

    roles\user managment

---
+++ StudentHub/backend/StudentHub.Api/Models/Roles/AssignRoleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentHub.Api.Models.Roles
+{
+    public class AssignRoleDto
+    {
+        [Required]
+        public string UserId { get; set; } = default!;
+
+        [Required]
+        public string Role { get; set; } = default!;
+    }
+}
---
+++ StudentHub/backend/StudentHub.Api/Models/Roles/UserWithRolesDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentHub.Api.Models.Roles
+{
+    public class UserWithRolesDto
+    {
+        public string Id { get; set; } = default!;
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+}
